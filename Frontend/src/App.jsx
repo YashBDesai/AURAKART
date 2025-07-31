46cdfa35946d833e8d41c1955c17404e23bb5622
krishna <krishnadesai20802@gmail.com>
2025-07-31 13:18:18 +0530

    Removed Commented code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,31 +1,3 @@
-// // import React from "react";
-// // import { Route, Routes } from "react-router-dom";
-// // import { Box } from "@chakra-ui/react";
-// // import CreatePage from "./components/Pages/Createpage";
-// // import ProductDetailsModal from "./components/Pages/ProductDetailsModal";
-// // import Navbar from "./components/Navbar";
-// // import HomePage from "./components/Pages/Homepage";
-// // import PageWrapper from "./store/pagewrapper";
-// // function App() {
-// //   return (
-// //     <Box
-// //       minH="100vh"
-// //       bgGradient="radial(125% 125% at 50% 10%, black 40%, #63e 100%)"
-// //       boxShadow="md"
-// //       borderRadius="md"
-   
-// //     >
-// //       <PageWrapper>
-// //       <Navbar />
-// //       <Routes>
-// //         <Route path="/" element={<HomePage />} />
-// //         <Route path="/create" element={<CreatePage />} />
-// //         <Route path="/product/:id" element={<ProductDetailsModal />} />
-// //       </Routes>
-// //       </PageWrapper>
-// //     </Box>
-// //   );
-// // }
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
